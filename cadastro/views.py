2d8b4f43b9f631fda3fa4275c526877e11f7d54c
MF-Guilherme <guimontenegro23@yahoo.com.br>
2024-05-08 18:32:02 -0300

    feat(cadastros):Template de cadastros em construção

--- cadastro/views.py
+++ cadastro/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Ciclo, Produto, Acabamento, Papel, Versao, Caderno
+from django.http import HttpResponse
 
-# Create your views here.
+
+def cadastrar_ciclo(request):
+    if request.method == 'GET':
+        return render(request, 'cadastro_ciclo.html')
