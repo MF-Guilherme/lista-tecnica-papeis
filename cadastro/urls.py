0f86cc235fd1fc403939f9d0660ed691a56dbd39
MF-Guilherme <guimontenegro23@yahoo.com.br>
2024-05-09 18:25:44 -0300

    feat(cadastro/lista_tecnica): rota criada e template em construção

--- cadastro/urls.py
+++ cadastro/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('cadastrar_ciclo/', views.cadastrar_ciclo, name='cadastrar_ciclo'),
     path('cadastrar_versao/', views.cadastrar_versao, name='cadastrar_versao'),
     path('cadastrar_acabamento/', views.cadastrar_acabamento, name='cadastrar_acabamento'),
-    path('cadastrar_papel/', views.cadastrar_papel, name='cadastrar_papel')
+    path('cadastrar_papel/', views.cadastrar_papel, name='cadastrar_papel'),
+    path('cadastrar_lista_tecnica/', views.cadastrar_lista_tecnica, name='cadastrar_lista_tecnica'),
 ]
